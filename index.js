7e9ee63dd53b075104762cddac9c23b14aca3a8a
Ishan Sandeepa <108740934+ishansandeepa757@users.noreply.github.com>
2022-07-17 08:07:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ async function startAshiya() {
         if (!Ashiya.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
         if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
         m = smsg(Ashiya, mek, store)
-        require("./Gojosensei")(Ashiya, m, chatUpdate, store)
+        require("./Queen_Ashiya")(Ashiya, m, chatUpdate, store)
         } catch (err) {
             console.log(err)
         }
