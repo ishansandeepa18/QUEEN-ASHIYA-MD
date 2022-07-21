c3e5a1a15fd8bbcb99f5883768f2af1d0322a200
Ishan Sandeepa <108740934+ishansandeepa757@users.noreply.github.com>
2022-07-20 21:48:02 -0700

    Update Queen_Ashiya.js

--- Queen_Ashiya.js
+++ Queen_Ashiya.js
@@ -238,7 +238,7 @@ const reply = (teks) => {
             console.log('Limit Reseted')
         }, {
             scheduled: true,
-            timezone: "Asia/Sri Lanka"
+            timezone: "Asia/Kolkata"
         })
         
 	//auto set bio\\
@@ -3474,7 +3474,7 @@ ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Obje
                 reply
 		
 		
-		(`I Am 💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞\n\n*My Owner Is ${global.ownername}*\n\nhttp://wa.me/${global.owner}`)
+		(`I Am 💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞\n\n*My Owner Is ${global.ownername}*\n\nhttp://wa.me/${global.owner}*\n\n ᴘᴏᴡᴇʀᴇᴅ ʙʏ  ɪꜱʜᴀɴ ꜱᴀɴᴅᴇᴇᴘᴀ footer: '💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞'`)
 		    
     
 }
